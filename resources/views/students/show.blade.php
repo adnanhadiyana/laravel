d768529c62fadff2727fe5d0cf589ef3ce42f296
adnanhadiyana <adnanhadiyana6@mail.unpas.ac.id>
2019-12-05 21:42:38 +0700

    selesai mungkin

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,28 +1,32 @@
 @extends('template/main')
 
-@section('title','detail mahasiswa')
+
+@section('judul', 'Detail Student')
 
 @section('container')
-
-
 <div class="container">
-<div class="row">
-<div class="col">
+ <div class="row">
+  <div class="col">
+      <h1>Detail Student</h1>
 
-<h1>detail mahasiswa</h1>
-<div class="card" style="width: 18rem;">
+      <div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">{{$student->nama}}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">{{$student->email}}</h6>
-    <p class="card-text">{{$student->nrp}}</p>
+    <h6 class="card-subtitle mb-2 text-muted">{{$student->nrp}}</h6>
+    <p class="card-text">{{$student->email}}</p>
     <p class="card-text">{{$student->jurusan}}</p>
-    <a href="#" class="card-link">edit</a>
-    <a href="#" class="card-link">hapus</a>
+    <a href="{{ $student->id }}/edit" class="btn btn-primary">Edit </a>
+
+    <form action="/students/{{ $student->id}}" method="post" class="display d-inline">
+    @method('delete')
+    @csrf
+    <button type="submit" class="btn btn-danger">Hapus </button>
+    </form>
     <a href="{{url('students/')}}" class="card-link">kembali</a>
   </div>
 </div>
-</div>
-</div>
-</div>
 
+  </div>
+ </div>
+</div>
 @endsection
